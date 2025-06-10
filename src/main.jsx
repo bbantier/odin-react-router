4b07664f36d0094b23a85ff4a175907e72eab087
bbantier <65014410+bbantier@users.noreply.github.com>
2025-06-10 08:57:23 +0200

    feat: add errorElement to home page

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
-import Profile from "./components/Profile.jsx";
+import Profile from "./components/Profile";
+import ErrorPage from "./components/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <ErrorPage />,
   },
   {
     path: "profile/:name",
